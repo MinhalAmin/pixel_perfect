384b23493b4e9553399825bf28d9b6569fa8a7a3
Minhal Mohammed Amin <minhalmohammedamin@gmail.com>
2024-10-06 11:43:48 -0700

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {};
 
 export default nextConfig;
 
-eslint: {
-    ignoreDuringBuilds: true;
-}
+module.exports = {
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+  }
